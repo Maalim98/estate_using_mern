7ec9d6e49352307891ca94cf2ba26112196ddc09
Maalim98 <ashrafzani585@gmail.com>
2024-02-06 10:06:26 +0300

    add pages nd route

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Home from './pages/Home';
+import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
+import Profile from './pages/Profile';
+import About from './pages/About';
 
 export default function App() {
-  return (
-    <h1 className='text-red-500'>App</h1>
-  )
+  return <BrowserRouter>
+  <Routes>
+  <Route path='/' element={<Home />} />
+  <Route path='/signup' element={<SignUp />} />
+  <Route path='/signin' element={<SignIn />} />
+  <Route path='/profile' element={<Profile />} />
+  <Route path='/About' element={<About />} />
+
+  </Routes>
+  
+  
+  
+  </BrowserRouter>
 }
